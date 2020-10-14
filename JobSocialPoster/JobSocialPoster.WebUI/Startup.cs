e1135183de706de4deb71354149e8017c9c134c8
wues86 <30496865+wues86@users.noreply.github.com>
2020-10-14 20:44:10 +0200

    Solution setup

---
+++ JobSocialPoster/JobSocialPoster.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JobSocialPoster.WebUI.Startup))]
+namespace JobSocialPoster.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
