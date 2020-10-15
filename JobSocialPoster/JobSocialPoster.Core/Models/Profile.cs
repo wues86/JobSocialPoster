521f216fa5d1ece00388a153e97cab9f6f8588ed
wues86 <30496865+wues86@users.noreply.github.com>
2020-10-15 16:21:39 +0200

    Data access update

---
+++ JobSocialPoster/JobSocialPoster.Core/Models/Profile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobSocialPoster.Core.Models
+{
+    public class Profile
+    {
+        [Key]
+        [DisplayName("Id profilu")]
+        public string Id { get; set; }
+        [DisplayName("Nazwa profilu")]
+        public string Name { get; set; }
+        [DisplayName("Plik CSV do importu postów")]
+        public string  File { get; set; }
+        [DisplayName("Id profilu w SocialPilot")]
+        public string SocialpilotId { get; set; }
+        [DisplayName("Zakończono eksport postów?")]
+        public bool IsSent { get; set; }
+        [DisplayName("Profil aktywny?")]
+        public bool IsActive { get; set; }
+        [DisplayName("Waga profilu")]
+        public int Weight { get; set; }
+        [Url]
+        [DisplayName("Adres URL profilu")]
+        public string Url { get; set; }
+        [DisplayName("Importować posty z CSV?")]
+        public bool ImportCsv { get; set; }
+        [DisplayName("Kategoria profilu")]
+        public string Category { get; set; }
+
+        public Profile()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+    }
+}
---
+++ JobSocialPoster/JobSocialPoster.DataAccess.InMemory/ProfileRepository.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using JobSocialPoster.Core.Models;
+
+
+namespace JobSocialPoster.DataAccess.InMemory
+{
+    public class ProfileRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Profile> profiles;
+
+        public ProfileRepository()
+        {
+            profiles = cache["profiles"] as List<Profile>;
+            if (profiles == null)
+            {
+                profiles = new List<Profile>();
+            }
+        }
+
+        public void Commit()
+        {
+            cache["profiles"] = profiles;
+        }
+
+        public void Insert(Profile p)
+        {
+            profiles.Add(p);
+        }
+        public void Update(Profile profile)
+        {
+            Profile profileToUpdate = profiles.Find(p => p.Id == profile.Id);
+
+            if (profileToUpdate != null)
+            {
+                profileToUpdate = profile;
+            }
+            else
+            {
+                throw new Exception("Profil nieznaleziony");
+            }
+
+        }
+        public Profile Find(string Id)
+        {
+            Profile profile = profiles.Find(p => p.Id == Id);
+
+            if (profile != null)
+            {
+                return profile;
+            }
+            else
+            {
+                throw new Exception("Profil nieznaleziony");
+            }
+        }
+
+        public IQueryable<Profile> Collection()
+        {
+            return profiles.AsQueryable();
+        }
+
+        public void Delete(string Id)
+        {
+            Profile profileToDelete = profiles.Find(p => p.Id == Id);
+
+            if (profileToDelete != null)
+            {
+                profiles.Remove(profileToDelete);
+            }
+            else
+            {
+                throw new Exception("Profil nieznaleziony");
+            }
+        }
+    }
+
+}
